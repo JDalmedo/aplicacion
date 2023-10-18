88f6a11975998e41460b7d85a0008b5153805adb
UsuarioM <jjdalmedo@gmail.com>
2023-10-18 13:57:27 +0200

    2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return(
         <>
             <div className='header'>
-                <h1>Trading.....</h1>
+                <h1>Lo que me gusta y más...</h1>
             </div>
         
         </>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,12 +6,28 @@ const Main = () => {
     return(
         <>
             <div className='main'>
-            <img src="public/indexado.png" alt="fondo_indexado"></img>
-            <img src="public/indexado.png" alt="fondo_indexado"></img>
-            <img src="public/indexado.png" alt="fondo_indexado"></img>
-            <img src="public/indexado.png" alt="fondo_indexado"></img>
-            <img src="public/indexado.png" alt="fondo_indexado"></img>
-            <img src="public/indexado.png" alt="fondo_indexado"></img>
+                <ul>
+                   <li>
+                        <img src="/fondoindexado.png" alt="fondo_indexado" />
+                          <p>Trading</p>
+                            <a href="null" alt="fondo" />
+                    </li>
+                    <li>
+                        <img src="/code.gif" alt="code" />
+                          <p>Código</p>
+                            <a href="null" alt="code." />
+                    </li>
+                    <li>
+                        <img src="/key.gif" alt="key" />
+                          <p>Calculadora de Par de Apriete</p>
+                            <a href="null" alt="key." />
+                    </li>
+                    <li>
+                        <img src="/Motor-4-tiempos.gif" alt="motor" />
+                          <p>Ciclo de motor de cuatro tiempos</p>
+                            <a href="null" alt="motor." />
+                    </li>
+                </ul>
             </div>
         </>
     );
