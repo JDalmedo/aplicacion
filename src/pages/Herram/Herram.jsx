cfb7551bcce6e80e1d296e81e3b6914a10615878
JDalmedo <jjdalmedo@gmail.com>
2023-10-24 20:45:31 +0200

    7

--- src/pages/Herram/Herram.jsx
+++ src/pages/Herram/Herram.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './Herram.css';
 
 const Herram = () => {
@@ -6,19 +7,19 @@ const Herram = () => {
                 <div className='container'>
                     <div className='sp500'>
                         <img src="/sp500.png" alt="sp500" />
-                            <a href="https://www.bbvatrader.com/bolsas-y-mercados-indices/indice/s-p-500/SP500/XCME">Trader</a>
+                            <NavLink href="https://www.bbvatrader.com/bolsas-y-mercados-indices/indice/s-p-500/SP500/XCME">Trader</NavLink>
                     </div>
                     <div className='code'>
                         <img src="/code.gif" alt="code" />
-                            <a href="https://thepower.education/bootcamp-fullstack">Código</a>
+                            <NavLink href="https://thepower.education/bootcamp-fullstack">Código</NavLink>
                     </div>
                     <div className='key'>
                         <img src="/key.gif" alt="key" />
-                            <a href="https://www.online-iso-calculator.com/Calculo_par_de_Apriete/index.php">Pares de Apriete</a>
+                            <NavLink href="https://www.online-iso-calculator.com/Calculo_par_de_Apriete/index.php">Pares de Apriete</NavLink>
                     </div>
                     <div className='motor'>
                         <img src="/Motor-4-tiempos.gif" alt="motor" />
-                            <a href="https://www.solediesel.com/es/motor-hibrido">Motores</a>
+                            <NavLink href="https://www.solediesel.com/es/motor-hibrido">Motores</NavLink>
                     </div>
 
                 </div>
