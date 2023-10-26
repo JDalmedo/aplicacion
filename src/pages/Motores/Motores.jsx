acefaadd17c5aa25db9546e53505e64404bcfe7d
JDalmedo <jjdalmedo@gmail.com>
2023-10-26 20:28:21 +0200

    12

--- src/pages/Motores/Motores.jsx
+++ src/pages/Motores/Motores.jsx
@@ -4,7 +4,10 @@ const Motores = () => {
     return (
         <>
             <div className='engine'>
-                <video controls src='src/assets/Funcionamiento.mp4' type='video/mp4'/>  
+                <video controls autoPlay>
+                    <source src='/video/Funcionamiento.mp4' type='video/mp4'/>
+                    <source src='/video/Funcionamiento.ogg' type='video/ogg'/>
+                </video>   
             </div>
         </>
     );    
